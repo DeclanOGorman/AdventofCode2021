1596e5724f9fd3f6dcfcd922d0ed22501fdc853b
Declan O'Gorman <dogorman@outlook.com>
2021-12-07 09:59:43 +0000

    Day 7

--- 7/code.py
+++ 7/code.py
@@ -5,7 +5,5 @@ with open('./7/input_a.txt', 'r') as f:
 fuel = sum([abs(a - int(statistics.median(input))) for a in input])
 print(f'Part A: fuel cost to align (fixed cost / median) - {fuel}')
 
-fuel = 9999999999
-for i in range(min(input), max(input)):
-    fuel = int(min(fuel, sum([abs(a - i)*(abs(a - i)+1)/2 for a in input])))
+fuel = int(min([sum([abs(a - i)*(abs(a - i)+1)/2 for a in input]) for i in range(min(input), max(input))]))
 print(f'Part B: fuel cost to align (increasing costs) - {fuel}')
