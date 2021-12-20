c76276449895d74012666a8114d506077fbfaba3
Declan O'Gorman <dogorman@outlook.com>
2021-12-20 22:00:23 +0000

    Board Render

---
+++ API_render/code.py
@@ -0,0 +1,26 @@
+# not perfect - but good enough for now, prints to console a view of day scores & completion times for star 2 vs 1 show averages and flag unlikely efforts
+
+from datetime import datetime
+from termcolor import colored  # https://pypi.org/project/termcolor/
+import json
+with open('./API_render/leaderboard.json') as f: 
+    board = json.loads(f.readline().strip())
+
+data = list()
+for m in board['members']:
+    member = [board['members'][m]['name'], board['members'][m]['local_score'], board['members'][m]['stars'],dict()]
+    for d in board['members'][m]['completion_day_level']:
+        stars = len(board['members'][m]['completion_day_level'][d])
+        t = int(board['members'][m]['completion_day_level'][d]['2']['get_star_ts']) - int(board['members'][m]['completion_day_level'][d]['1']['get_star_ts']) if stars == 2 else 0
+        member[3][int(d)-1] = [stars, t] #datetime.utcfromtimestamp(t).strftime('%H:%M:%S')]
+    data.append(member)
+
+def sortKey(m): return m[1]
+data.sort(key=sortKey, reverse=True) #sort by local rank
+def star(num): return colored('*', 'yellow' if num == 2 else 'white')   #''.join(['*' for a in range(num)]) # Show stars rather than numbers * / **
+
+print('{0:<3}   {1:<31} {2}'.format('#', 'Name (Stars)', ''.join(['{:<8} '.format(str(a)) for a in range(1, 25)])))
+for rank in range(len(data)): 
+    res = ''.join([''.format(a) for a in data[rank][3]])
+    print('{:<3} - {:<25} ({:<2}) '.format(rank+1, str(data[rank][0]), data[rank][2]), 
+        ''.join(['{0:<2} {1:<12} '.format(star(data[rank][3][a][0]), colored(data[rank][3][a][1], 'red' if 0 < data[rank][3][a][1] < 90 else 'white')) for a in data[rank][3]]))
